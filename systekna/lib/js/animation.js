18064b1ab711de92f0f191d2ce9b3b7fe98a5132
CELSO DE ALMEIDA <celso.almeida.leite@hotmail.com>
2023-08-30 21:28:27 -0300

    update: project html.

--- systekna/lib/js/animation.js
+++
@@ -1,8 +0,0 @@
-gsap.registerPlugin(ScrollTrigger);
-
-let tl = gsap.timeline({delay: 2});
-
-tl.from("#mainNav .navbar-brand",{ duration: 1.2 ,xPercent: -280 ,ease: "power2.out" ,opacity: 0 ,scale: 0.5  }, "+=0.5")
-tl.from("#mainNav ul",{duration: 2 ,yPercent: -280 ,ease: "power2.out" ,opacity: 0 ,scale: 0.5 }, "+=0.5")
-
-tl.restart();
